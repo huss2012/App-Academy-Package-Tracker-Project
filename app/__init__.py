13c9968384c7ced59947efb346df34c0f0698f1c
huss2012 <huss24940@gmail.com>
2024-12-04 04:31:52 +0300

    Add DB and create package model amd migrate it to the DB.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
 from .config import Config
 from .routes import index, new_package
+from flask_migrate import Migrate
+from .models import db
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.register_blueprint(index.bp)
 app.register_blueprint(new_package.bp)
+db.init_app(app)
+Migrate(app, db)
