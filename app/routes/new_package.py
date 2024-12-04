c61c88ab0d8df75d307b69b211a213f4d8d4d9dc
huss2012 <huss24940@gmail.com>
2024-12-04 06:21:35 +0300

    Adding a record to the database package table.

--- app/routes/new_package.py
+++ app/routes/new_package.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect
 from ..forms.shipping_form import ShippingForm
-
+from app.models import db, Package
 
 
 bp = Blueprint("new_package", __name__, url_defaults="")
@@ -10,8 +10,16 @@ def new_package():
     shipping_form = ShippingForm()
 
     if shipping_form.validate_on_submit():
-        print(f'{shipping_form.data}')
-        return redirect('/new_package')
+        data = shipping_form.data
+        new_package = Package(sender= data['sender_name'],
+                              recipient=data['recpient_name'],
+                              origin=data['origin'],
+                              destination=data['destination'],
+                              location=data['origin']
+                              )
+        db.session.add(new_package)
+        db.session.commit()
+        return redirect('/')
 
 
     return render_template("shipping_request.html", shipping_form = shipping_form)
