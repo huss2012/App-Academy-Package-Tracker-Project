b8cc111a4cc92d5d827defbd8d0f0e208350a729
huss2012 <huss24940@gmail.com>
2024-12-04 06:57:56 +0300

    Adding package status page and render the package from the database.

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
+from app.models import db, Package
 
 bp = Blueprint("index", __name__, url_defaults="")
 
 @bp.route("/", methods=["GET", "POST"])
 def index():
+    packages = Package.query.all()
+
+    if packages:
+        return render_template("package_status.html", packages=packages)
+
     return render_template("index.html")
